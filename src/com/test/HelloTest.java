43970a6ca8f6b6898cd2928d9dd33a881e044431
Zgpzero <1067738882@qq.com>
2022-04-26 21:17:24 +0800

    hot-fix  second commit

--- src/com/test/HelloTest.java
+++ src/com/test/HelloTest.java
@@ -5,5 +5,6 @@ public class HelloTest {
         System.out.println("hello world!");
         System.out.println("hello git");
         System.out.println("hot-fix");
+        System.out.println("hot-test");
     }
 }
